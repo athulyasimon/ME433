c5afbd81b3e844e44174157de63734e7ea4f0ef4
Athulya Simon <AthulyaSimon2014@u.northwestern.edu>
2015-06-09 20:38:08 -0500

    added flashlight app

---
+++ Hmwk 10/Flashlight/app/src/main/java/com/example/athulya/flashlight/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.athulya.flashlight;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    //flag to detect flash is on or off
+    private boolean isLighOn = false;
+
+    private Camera camera;
+
+    private Button button;
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (camera != null) {
+            camera.release();
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.buttonFlashlight);
+
+        Context context = this;
+        PackageManager pm = context.getPackageManager();
+
+        // if device support camera?
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            Log.e("err", "Device has no camera!");
+            return;
+        }
+
+        camera = Camera.open();
+        final Parameters p = camera.getParameters();
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                if (isLighOn) {
+
+                    Log.i("info", "torch is turn off!");
+
+                    p.setFlashMode(Parameters.FLASH_MODE_OFF);
+                    camera.setParameters(p);
+                    camera.stopPreview();
+                    isLighOn = false;
+
+                } else {
+
+                    Log.i("info", "torch is turn on!");
+
+                    p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+
+                    camera.setParameters(p);
+                    camera.startPreview();
+                    isLighOn = true;
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
